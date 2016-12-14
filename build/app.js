0b0ddd8d5c20579a2e29c9edb17ba5d1535f9ea1
rcsuax <rcsuax@gmail.com>
2016-12-14 22:51:13 +0000

    build/

---
+++ build/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _mongoose = require('mongoose');
+
+var _mongoose2 = _interopRequireDefault(_mongoose);
+
+var _routes = require('./routes');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+var port = process.env.PORT || 8080;
+
+app.use(_routes.router);
+
+var dbUrl = process.env.DB || '127.0.0.1:27017/test';
+
+_mongoose2.default.connect(dbUrl);
+var db = _mongoose2.default.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log('mongo connected');
+    console.log(dbUrl);
+});
+
+app.listen(port, function () {
+    console.log('Express listening at port: ' + port);
+});
